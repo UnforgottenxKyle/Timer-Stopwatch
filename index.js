be2040488c2c10e64a3cbd35b1c172d0c9eb08df
Grifin Kyle Asa <grifin4kyle@gmail.com>
2024-08-17 23:46:03 +0800

    latest stopwatch fix the pause button

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-//time
+//Time
 const labelMinutes = document.querySelector("#minutes");
 const labelSeconds = document.querySelector("#seconds");
 const labelMilliseconds = document.querySelector("#milliseconds");
-//button
+
+//Button
 let startButton = document.querySelector("#startBtn");
 let stopButton = document.querySelector("#stopBtn");
 let pauseButton = document.querySelector("#pauseBtn");
 let resetButton = document.querySelector("#resetBtn");
-//variables
+
+//lapTime ul
+const laplist = document.querySelector("#laplist");
+
+//Variables
 let minutes = 0;
 let seconds = 0;
 let milliseconds = 0;
 let interval;
+
 //start Button
 const startBtn = () => {
   interval = setInterval(realTime, 10);
   startButton.disabled = true;
 };
+
+//Stop Button
+
+const stopBtn = () => {
+  clearInterval(interval);
+  addLapList();
+  minutes = 0;
+  seconds = 0;
+  milliseconds = 0;
+  displayRealTime();
+  startButton.disabled = false;
+};
+
 //Pause Button
 const pauseBtn = () => {
   clearInterval(interval);
   pauseBtn.disabled = true;
+  startButton.disabled = false;
 };
 //Reset Button
 const resetBtn = () => {
@@ -48,4 +68,15 @@ const displayRealTime = () => {
   labelMilliseconds.textContent = milliseconds.toString().padStart(2, "0");
   labelSeconds.textContent = seconds.toString().padStart(2, "0");
   labelMinutes.textContent = minutes.toString().padStart(2, "0");
+};
+const addLapList = () => {
+  const lapTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}:${milliseconds.toString().padStart(2, "0")}`;
+
+  const liItem = document.createElement("li");
+  liItem.innerHTML = `<span>Lap ${
+    laplist.childElementCount + 1
+  }: </span>${lapTime}`;
+  laplist.appendChild(liItem);
 };
